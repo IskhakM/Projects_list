6d7a3fb36970abe5a632e3495dc7dfd7a2d0204f
Исхак Мурзаев <138295514+IskhakM@users.noreply.github.com>
2025-11-17 12:56:45 +0600

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-
-def get_unique_short_id():
-    chars = string.ascii_letters + string.digits
-    while True:
-        short_id = ''.join(random.choice(chars) for _ in range(6))
-        return short_id
